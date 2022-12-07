3a70b68732c20d9e4cce030c4c7023e68c257039
Jan Kneissler <jan.kneissler@breuninger.de>
2022-12-07 20:43:38 +0100

    add noauth policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,6 +80,23 @@ resource "google_cloud_run_service" "service" {
   }
 }
 
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.service.location
+  project     = google_cloud_run_service.service.project
+  service     = google_cloud_run_service.service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
 terraform {
   backend "gcs" {
     bucket  = "gitbeaver-terraform-state"
